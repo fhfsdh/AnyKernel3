7a3e9085b5337dd14ba49b9a3ea1064cef7bd337
fhfsdh <zhaodewen0721@163.com>
2025-11-13 14:39:56 +0800

    Add AnyKernel3 ramdisk mod script

---
+++ anykernel.sh
@@ -0,0 +1,39 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=ExampleKernel by osm0sis @ xda-developers # 请自行填写内核名字
+do.devicecheck=0 # 关闭了设备检测
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+# boot shell variables
+block=boot;  # 刷写分区是boot
+is_slot_device=auto; # 自动检测是否AB插槽
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
